b2c5c6391266f48a6f459acd8488c2927fac79f4
WillyJL <49810075+Willy-JL@users.noreply.github.com>
2021-09-22 19:52:57 +0200

    Update codestats_box.py

--- codestats_box.py
+++ codestats_box.py
@@ -42,13 +42,13 @@ VALUE_FORMAT = {
     STATS_TYPE_XP: "{xp:>9,} XP",
 }
 GIST_TITLE = {
-    STATS_TYPE_LEVEL: "💻 My Code::Stats XP (Top Languages)",
-    STATS_TYPE_RECENT_XP: "💻 My Code::Stats XP (Recent Languages)",
-    STATS_TYPE_XP: "💻 My Code::Stats XP (Top Languages)",
+    STATS_TYPE_LEVEL: "🧩 Code::Stats Levels",
+    STATS_TYPE_RECENT_XP: "🧩 Recent Code::Stats",
+    STATS_TYPE_XP: "🧩 Code::Stats XP",
 }
 # Other configurable values
 TOP_LANGUAGES_COUNT = 10
-WIDTH_JUSTIFICATION_SEPARATOR = ":"
+WIDTH_JUSTIFICATION_SEPARATOR = " "
 RECENT_STATS_SEPARATOR = " + "
 TOTAL_XP_TITLE = "Total XP"
 NO_RECENT_XP_LINES = [
